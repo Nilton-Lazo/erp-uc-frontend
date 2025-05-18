55a577bc3f81e14837cdd28a1198729b11f756b9
Nilton Lazo <74091904@continental.edu.pe>
2025-05-18 12:58:33 -0500

    Primer commit de inicio

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+      <div className="h-screen bg-green-800 text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold">TAILWIND ESTÁ FUNCIONA</h1>
+      </div>     
+  )
+}
+
+export default App
